8554c21b27c7c37ccd4768203bc2af3e630b92fb
Keanan McDonald <keananmcdonald5@gmail.com>
2023-05-18 14:21:27 +0100

    Tests created for service

--- src/services/storage.service.spec.ts
+++ src/services/storage.service.spec.ts
@@ -21,6 +21,7 @@ describe('StorageService', () => {
     expect(service).toBeTruthy();
   });
 
+  // Testing the GET
   it('should get data from database', () => {
     const responseData = [{ _id: '1', logNumber: 1, owner: "TestOwner", make: "TestMake", model: "TestModel", type: "TestType", serialNumber: "TestSerialNumber" },
     { _id: '2', logNumber: 2, owner: "TestOwner2", make: "TestMake2", model: "TestModel2", type: "TestType2", serialNumber: "TestSerialNumber2" }];
@@ -38,6 +39,80 @@ describe('StorageService', () => {
     });
 
     expect(httpClient.get).toHaveBeenCalledWith(service.endpoint + '/', {
+      headers: new HttpHeaders().set('Access-Control-Allow-Origin', '*')
+    });
+  });
+
+  // Testing the POST
+  it('should send a POST request with the correct endpoint, firearm, and headers', () => {
+    const firearm = {
+      id: '1', 
+      logNumber: 1, 
+      owner: "TestOwner", 
+      make: "TestMake", 
+      model: "TestModel", 
+      type: "TestType", 
+      serialNumber: "TestSerialNumber"
+    };
+
+    spyOn(httpClient, 'post').and.returnValue(of({}));
+
+    service.addFirearm(firearm).subscribe();
+
+    expect(httpClient.post).toHaveBeenCalledWith(service.endpoint + '/create', firearm, {
+      headers: new HttpHeaders().set('Access-Control-Allow-Origin', '*')
+    });
+  });
+  
+  // Testing the DELETE
+  it('should send a DELETE request', () => {
+    const firearm = {
+      id: '1',
+      logNumber: 1, 
+      owner: "TestOwner", 
+      make: "TestMake", 
+      model: "TestModel", 
+      type: "TestType", 
+      serialNumber: "TestSerialNumber"
+    };
+
+    spyOn(httpClient, 'delete').and.returnValue(of({}));
+
+    service.deleteFirearm(firearm).subscribe();
+
+    expect(httpClient.delete).toHaveBeenCalledWith(service.endpoint + '/' + firearm.id, {
+      headers: new HttpHeaders().set('Access-Control-Allow-Origin', '*')
+    })
+  });
+
+  // Testing the PUT
+  it('should send a PUT request with the correct endpoint, firearm, and headers', () => {
+    const firearm = {
+      id: '1',
+      logNumber: 1,
+      owner: 'TestOwner',
+      make: 'TestMake',
+      model: 'TestModel',
+      type: 'TestType',
+      serialNumber: 'TestSerialNumber'
+    };
+  
+    const editURL = service.endpoint + '/' + firearm.id;
+  
+    const expectedBody = {
+      logNumber: firearm.logNumber,
+      owner: firearm.owner,
+      make: firearm.make,
+      model: firearm.model,
+      type: firearm.type,
+      serialNumber: firearm.serialNumber
+    };
+  
+    spyOn(httpClient, 'put').and.returnValue(of({}));
+  
+    service.editFirearm(firearm).subscribe();
+  
+    expect(httpClient.put).toHaveBeenCalledWith(editURL, expectedBody, {
       headers: new HttpHeaders().set('Access-Control-Allow-Origin', '*')
     });
   });
